b2dc54a91fbadb2130a56aae1d56790a6656c28a
Thomas Reynoso <thomas.reynoso@codetria.com>
2022-02-21 22:06:56 -0300

    se agrega kick spongebob

---
+++ kickSpongeBob.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix='&')
+
+@bot.event
+async def on_message(msg):
+    if "858060188119597087/858060748256575488" in msg.content:
+        await kick(msg, msg.author)
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def kick(ctx, member: discord.Member):
+    await member.kick()
+    await ctx.message.add_reaction("reaction")
+
+bot.run('OTAyNzA2OTQ5MDM1MDEyMTE4.YXiVXA.y6rb5FBWdqtVg_LIh3rl1c6pVFU')
