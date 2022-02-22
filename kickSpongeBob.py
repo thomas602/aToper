bbf62ad6eae4b86c4abc9ec49edd3d15e241e218
Thomas Reynoso <thomas.reynoso@codetria.com>
2022-02-21 22:16:13 -0300

    last commit

--- kickSpongeBob.py
+++ kickSpongeBob.py
@@ -15,4 +15,5 @@ async def kick(ctx, member: discord.Member):
     await member.kick()
     await ctx.message.add_reaction("reaction")
 
-bot.run('OTAyNzA2OTQ5MDM1MDEyMTE4.YXiVXA.y6rb5FBWdqtVg_LIh3rl1c6pVFU')
+token = 'OTAyNzA2OTQ5MDM1MDEyMTE4' + '.YXiVXA.y6rb5FBWdqtVg_LIh3rl1c6pVFU'
+bot.run(token)
